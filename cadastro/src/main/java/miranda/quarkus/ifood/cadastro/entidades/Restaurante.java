08acd1081349541e3cab151616d9488812cc2f2d
Ewerton Cordeiro Miranda <sonardevplus@gmail.com>
2022-11-19 14:36:43 -0300

    Dependencia do Lombok

---
+++ cadastro/src/main/java/miranda/quarkus/ifood/cadastro/entidades/Restaurante.java
@@ -0,0 +1,10 @@
+package miranda.quarkus.ifood.cadastro.entidades;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Restaurante extends PanacheEntityBase {
+}
