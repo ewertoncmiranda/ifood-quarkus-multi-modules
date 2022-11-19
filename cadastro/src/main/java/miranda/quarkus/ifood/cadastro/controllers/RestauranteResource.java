09a25cd16b9bf63ca58dfb1de1c0f5d4e80dc6f6
Ewerton Cordeiro Miranda <sonardevplus@gmail.com>
2022-11-19 15:38:28 -0300

    endpoint de delete

--- cadastro/src/main/java/miranda/quarkus/ifood/cadastro/controllers/RestauranteResource.java
+++ cadastro/src/main/java/miranda/quarkus/ifood/cadastro/controllers/RestauranteResource.java
@@ -3,6 +3,7 @@ package miranda.quarkus.ifood.cadastro.controllers;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import miranda.quarkus.ifood.cadastro.entidades.Restaurante;
 
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -20,18 +21,26 @@ public class RestauranteResource {
     }
 
     @POST
-    @Path("/{id}")
+    @Transactional
     public void adicionarRestaurante(Restaurante dto){
         dto.persist();
     }
 
     @PUT
-    @Path("/{ID}")
+    @Path("/{id}")
     public boolean atualizar(@PathParam("id") Long id, Restaurante dto){
+
         Optional<Restaurante> restauranteOpt = Restaurante.findByIdOptional(id);
+
         if(restauranteOpt.isEmpty()) throw new NotFoundException();
         PanacheEntityBase restaurante = restauranteOpt.get();
         return  true;
+    }
+    @DELETE
+    @Path("/{id}")
+    public boolean deletar(@PathParam("/{id}") Long id){
+        if( !Restaurante.findByIdOptional(id).isEmpty()) Restaurante.deleteById(id);
+        throw new NotFoundException();
     }
 
 
